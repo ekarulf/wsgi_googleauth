16ab8e64e49e516ab2aad43fbae83cd0b2c15814
Erik Karulf <erik@karulf.com>
2010-01-18 00:30:33 -0600

    Updating to the new buildout system  * User places the auth methods in config/policy.py  * Added an example policy file  * Forced to create a noop method as an entry point

---
+++ config/example.py
@@ -0,0 +1,12 @@
+#!/usr/local/bin/python
+
+from wsgi_googleauth import GoogleAuth
+from wsgi_googleauth.decorators import *
+
+backend = GoogleAuth()
+
+@RequireDomain("example.org")
+@Cache(timeout=60*15) # 15 minutes
+def check_password(environ, username, password):
+    return backend.check_password(environ, username, password)
+
---
+++ src/wsgi_googleauth/commands.py
@@ -0,0 +1,8 @@
+# Copyright 2009 - Erik Karulf - MIT License - See LICENSE.txt
+
+def noop():
+  """
+  No Operation - zc.buildout requires an endpoint method
+  """
+  pass
+
